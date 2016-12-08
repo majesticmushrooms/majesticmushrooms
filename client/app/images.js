f231d4075e5822e77e634440e268545a55b115c2
Layne Miao <layne00@gmail.com>
2016-12-07 20:14:54 -0800

    added functions for flickr api

---
+++ client/app/images.js
@@ -0,0 +1,43 @@
+app.factory('Images', function() {
+  var flickr = new Flickr({
+      api_key: "d1f8ae89e54867864f4acbf48d347518"
+    });
+ 
+  var getImagesFlickr = function(text) {
+    return new Promise (function (resolve, reject) {
+      flickr.photos.search({
+        text: text, 
+        has_geo: 1
+      }, function(err, result) {
+        if(err) { reject(err); }
+        console.log(result.photos.photo)
+        var results = result.photos.photo.map(imageObject => {
+          var ourImage = {};
+          ourImage.url = 'https://farm'+imageObject.farm+'.staticflickr.com/'+ imageObject.server + 
+                           '/'+ imageObject.id + '_' + imageObject.secret + '.jpg';
+          ourImage.id = imageObject.id;
+          return ourImage
+        })
+        console.log(results);
+        resolve(results);
+      })
+    })
+  }
+
+  var getImageLocation = function(id) {
+    return new Promise (function (resolve, reject) {
+      flickr.photos.geo.getLocation({
+        photo_id: id
+      }, function(err, result) {
+        if(err) { reject(err); }
+        resolve(result);
+      });
+    });
+  }
+
+
+  return {
+    getImagesFlickr: getImagesFlickr,
+    getImageLocation: getImageLocation
+  }
+});
