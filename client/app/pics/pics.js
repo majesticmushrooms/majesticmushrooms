c2fe3289f708ca432f4ce7b45ff842dc348e2e90
Autumn Smith <autumn07@gmail.com>
2016-12-08 18:36:15 -0800

    almost got angular to work

--- client/app/pics/pics.js
+++ client/app/pics/pics.js
@@ -1,20 +1,23 @@
-angular.module('app.pics', [])
-.controller('app.pics', function ($scope, Images) {
-  $scope.pictures = '';
-  $scope.selectedImageUrl = '';
+var pics = angular.module('app.pics', []);
 
+pics.controller('picController', function ($scope, Images) {
+  // $scope.pictures = 'hello';
+  $scope.selectedImageUrl = '';
+  $scope.test = 'abc'//['a', 'b']
   $scope.getImagesFlickr = function (searchBarText) {
     console.log('clicked');
     Images.getImagesFlickr(searchBarText).then(
     function(imageArray){
       //set images from helper fn to pictures scope var
       $scope.pictures = imageArray;
+      //$scope.apply();
       console.log('pictures', $scope.pictures);
-
     });
   };
 
   $scope.getImagesFlickr('hong kong');
+  console.log('pictures', $scope.pictures);
+
 
   $scope.addInfoToDB = function($index) {
     //pass in image object to add to the database
@@ -27,4 +30,9 @@ angular.module('app.pics', [])
       Images.postToServer(imageObject);
     });
   };
+
+ $scope.test = function(){
+   return $scope.pictures;
+ }
+
 });
