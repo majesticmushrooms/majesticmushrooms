74207a857377268959bc1c65f88d28adb1680408
Paige <paige.vogenthaler@gmail.com>
2016-12-12 13:56:41 -0800

    Fix bracket

--- client/app/pics/pics.js
+++ client/app/pics/pics.js
@@ -63,15 +63,15 @@ pics.controller('picController', function ($scope, $http, Images) {
     });
 
 
-  $scope.enlarge = function(url) {
-    $scope.modalUrl = url;
-    console.log('clicked enlarge', $scope.modalUrl);
-    $('#myModal').modal('show');
-  };
+    $scope.enlarge = function(url) {
+      $scope.modalUrl = url;
+      console.log('clicked enlarge', $scope.modalUrl);
+      $('#myModal').modal('show');
+    };
 
   // $scope.populatePictures(pictures) {
   //   return;
   // };
-
+  };
 
 });
