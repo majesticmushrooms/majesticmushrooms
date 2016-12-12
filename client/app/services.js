3c0b58e80d0377323950ae47a4c301ebdc58d485
Justin Potter <juspotter@gmail.com>
2016-12-11 20:07:05 -0800

    Merged updates from upstream master

--- client/app/services.js
+++ client/app/services.js
@@ -22,8 +22,8 @@ services.service('Geocoder', function() {
 
 services.factory('Images', function() {
   var flickr = new Flickr({
-    api_key: 'd1f8ae89e54867864f4acbf48d347518'
-  });
+      api_key: 'd1f8ae89e54867864f4acbf48d347518'
+    });
 
   var getImagesFlickr = function(text) {
     return new Promise (function (resolve, reject) {
@@ -31,38 +31,37 @@ services.factory('Images', function() {
         text: text,
         has_geo: 1
       }, function(err, result) {
-        if (err) { reject(err); }
-        console.log(result.photos.photo);
+        if(err) { reject(err); }
         var results = result.photos.photo.map(imageObject => {
           var ourImage = {};
-          ourImage.url = 'https://farm' + imageObject.farm + '.staticflickr.com/' + imageObject.server + '/' + imageObject.id + '_' + imageObject.secret + '.jpg';
+          ourImage.url = 'https://farm'+imageObject.farm+'.staticflickr.com/'+ imageObject.server +
+                           '/'+ imageObject.id + '_' + imageObject.secret + '.jpg';
           ourImage.id = imageObject.id;
-          return ourImage;
-        });
-        console.log(results);
+          return ourImage
+        })
         resolve(results);
-      });
-    });
-  };
+      })
+    })
+  }
 
   var getImageLocation = function(id) {
     return new Promise (function (resolve, reject) {
       flickr.photos.geo.getLocation({
         photo_id: id
       }, function(err, result) {
-        if (err) { reject(err); }
+        if(err) { reject(err); }
         var output = {
           longitude: result.photo.location.longitude,
           latitude: result.photo.location.latitude
-        };
+        }
         resolve(output);
       });
     });
-  };
+  }
 
 
   return {
     getImagesFlickr: getImagesFlickr,
     getImageLocation: getImageLocation
-  };
+  }
 });
