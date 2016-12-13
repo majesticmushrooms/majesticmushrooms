82ec907562e461bd6c5bc65575df93d74e69d54b
Layne Miao <layne00@gmail.com>
2016-12-12 16:31:24 -0800

    changed messageing

--- client/app/resources/facebook.js
+++ client/app/resources/facebook.js
@@ -23,9 +23,10 @@
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      //testAPI();
-      document.getElementById('status').innerHTML =
-        'Welcome to Around80!';
+      testAPI();
+
+      // document.getElementById('status').innerHTML =
+      //   'Welcome to Around80!';
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
@@ -59,10 +60,10 @@
 
 
   function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
+    //console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+      //console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        'Welcome to Around 80, ' + response.name + '!';
     });
   }
